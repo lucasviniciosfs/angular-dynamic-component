b015ddf8359d7c4100b11a94d111d05dbb5ff1a0
lucasviniciosfs <lucasvinicios-fs@hotmail.com>
2021-02-19 14:09:46 -0300

    hero profile component

---
+++ src/app/hero-profile/hero-profile.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-profile',
+  templateUrl: './hero-profile.component.html',
+  styleUrls: ['./hero-profile.component.css']
+})
+export class HeroProfileComponent implements OnInit {
+
+  @Input() data: any;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
